d9371859f00e431264567f7a5b1aaec6f718834b
kamiazya <yuki@kamiazya.tech>
2019-10-26 10:55:18 +0900

    fix code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { inspect } from 'util';
-import { KubeConfig } from '@kubernetes/client-node';
+import { KubeConfig, AppsV1Api } from '@kubernetes/client-node';
 
-const config = new KubeConfig();
-config.loadFromDefault();
-console.log(inspect(config));
+async function main() {
+  try {
+    const config = new KubeConfig();
+    config.loadFromDefault();
+
+    const appsV1Api = config.makeApiClient(AppsV1Api);
+    const resources = await appsV1Api.getAPIResources();
+
+    console.log(inspect(resources));
+    for (const resource of resources.body.resources) {
+      console.log(resource.name);
+
+    }
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+main();
